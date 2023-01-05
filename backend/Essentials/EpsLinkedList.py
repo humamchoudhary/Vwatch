1ee2e02161d820efffc53da4e5d0ac4bf41eae48
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-01-05 15:07:27 +0500

    update

--- backend/Essentials/EpsLinkedList.py
+++ backend/Essentials/EpsLinkedList.py
@@ -28,11 +28,19 @@ class LinkedList:
     def prevep(self):
         pass
 
-    def nextep(self):
+    def nextep(self,eps_no,token,profile):
+        """
+            - Find the current Eps in link list
+            - Search next eps in database
+            - Complete The current eps
+        """
         return self.head.next.data["url"]  #ik its wrong just checking if the func ca be called
 
-    def complete(self):
+    def complete(self,eps_no,token,profile):
         pass          
 
 
 
+if __name__=="__main__":
+    list = LinkedList()
+    
