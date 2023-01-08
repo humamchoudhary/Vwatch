7bf3b1ac0ec2e994de0130ee477a68f08359d815
Muhammad Zaryab <muhammadzaryab002@gmail.com>
2023-01-08 18:57:11 +0500

    a lil bit GenreTree

--- backend/Essentials/GenreTree.py
+++ backend/Essentials/GenreTree.py
@@ -3,3 +3,41 @@ import random
 import json
 import pickle
 from tinydb import TinyDB, Query
+
+
+
+class Node:
+
+    def __init__(self , name , parent = None ):
+
+        self.data = name
+        self.parent = parent
+        
+
+
+class Genre:
+    
+    def __init__(self):
+        self.root = None
+
+
+    def make_genre_mov(self):
+
+        Action = Node( "Action" , parent = mov_gen)
+        Adventure = Node( "Action" , parent = mov_gen)
+        Science_Fiction = Node( "Action" , parent = mov_gen)
+        Fantasy = Node( "Action" , parent = mov_gen)
+        Drama = Node( "Action" , parent = mov_gen)
+        Thriller = Node( "Action" , parent = mov_gen)
+        Horror = Node( "Action" , parent = mov_gen)
+        Mystery = Node( "Action" , parent = mov_gen)
+        Family = Node( "Action" , parent = mov_gen)
+        War = Node( "Action" , parent = mov_gen) 
+        Comedy = Node( "Action" , parent = mov_gen)
+        Romance = Node( "Action" , parent = mov_gen)
+        Music = Node( "Action" , parent = mov_gen)
+        Animation = Node( "Action" , parent = mov_gen)
+        
+
+
+mov_gen = Node("Movies")
